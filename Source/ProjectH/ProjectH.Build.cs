2c10ece27da5427ee037af9ef6b7e2ca11bd2102
seocom <wjddnjs6520@naver.com>
2025-09-11 10:48:52 +0900

    Initial commit: Added Unreal Engine project with basic module setup including Core, Engine, Input, GAS, and AI dependencies.

---
+++ Source/ProjectH/ProjectH.Build.cs
@@ -0,0 +1,38 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class ProjectH : ModuleRules
+{
+	public ProjectH(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { 
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore", 
+			"EnhancedInput",
+			
+			// GAS
+			"GameplayAbilities",
+			"GameplayTasks",
+			"GameplayTags",
+		
+			// AI
+			"AIModule",
+			"NavigationSystem"
+        });
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/ProjectH/ProjectH.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectH.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectH, "ProjectH" );
